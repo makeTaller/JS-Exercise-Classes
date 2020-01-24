b3a7447fbb0b71e27c6280f3a3829214ba585d21
Kirk Tolliver <kirktolliver28@gmail.com>
2020-01-24 07:05:43 -0600

    Initial commit

--- index.js
+++ index.js
@@ -41,7 +41,24 @@ class Airplane {
 */
 
 class Person {
+	constructor(name,age){
+	this.name = name;
+	this.age = age;
+	this.stomach = [];
+	}
 
+    eat(food){
+    this.stomach.length < 10 ? this.stomach.push(food): console.log('This is too much food!');
+    }
+
+
+    poop(){
+    return this.stomach = [];
+    }
+
+    toString() {
+    return `${this.name}, ${this.age}`
+    }
 }
 
 /*
