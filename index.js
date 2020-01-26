4b44e92087dd422287fd4a16aa8abc0f337e0394
Kirk Tolliver <kirktolliver28@gmail.com>
2020-01-26 07:31:38 -0600

    Finised with MVP

--- index.js
+++ index.js
@@ -213,7 +213,20 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor {
+	constructor(make){
+	super(make);
+	this.gradClassName = make.gradClassName;
+	this.favInstructor = make.favInstructor;
+	}
+
+	standUp(slack){
+	return `${this.name} announces to ${slack}, @channel standy times!`
+	}
+
+	debugsCode(student, subject){
+	return `${this.name} debugs ${student.name}'s code on ${subject}`
+	}
 
 }
 
